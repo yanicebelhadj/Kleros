b1091e5eaea66065645ba85daa6868f1829d6960
yanicebelhadj <yanice78955@gmail.com>
2023-04-11 19:07:11 +0200

    Publication and Actualite in progress

--- src/components/SavoirFaireItem/SavoirFaireItem.jsx
+++ src/components/SavoirFaireItem/SavoirFaireItem.jsx
@@ -1,16 +1,35 @@
+import { useState } from "react"
 import Arrow from "../../icons/arrowInCircle.svg"
 import "./SavoirFaireItem.css"
 
 function SavoirFaireItem(props){
+    const [active,setActive] =useState(false)
     return(
-        <div className="SavoirFaireItem row">
-            <div className="savoirFaireImage" style={{backgroundImage: `url(${props.image})`, backgroundPosition: "center center"}}></div>
-            <p className="p-l-semi-bold title">{props.title}</p>
-            <p className="p-m-regular description">{props.description}</p>
-            <div className="row enSavoirPlusContainer">
-                <p className="p-l-medium enSavoirPlus">En savoir plus</p>
-                <img src={Arrow} alt="En savoir plus" />
+        <div>
+            <div className="SavoirFaireItem row">
+                <div className="savoirFaireImage" style={{backgroundImage: `url(${props.image})`, backgroundPosition: "center center"}}></div>
+                <p className="p-l-semi-bold title">{props.title}</p>
+                {
+                    props.description.length <= 130 ?
+                    <p className="p-m-regular description">{props.description}</p>
+                    :
+                    <p className="p-m-regular description">{props.description.substr(0, 130)} ...</p>                
+                }
+                <div onClick={() => setActive(true)} className="row enSavoirPlusContainer">
+                    <p className="p-l-medium enSavoirPlus">En savoir plus</p>
+                    <img src={Arrow} alt="En savoir plus" />
+                </div>
             </div>
+            {
+                active &&
+                <div className="modalSavoirFaire">
+                    <div>
+                        <p>{props.title}</p>
+                        <p onClick={() => setActive(false)}>X</p>
+                    </div>
+                    <p>{props.description}</p>
+                </div>
+            }
         </div>
     )
 }
--- src/routes/SavoirFaire/SavoirFaire.jsx
+++ src/routes/SavoirFaire/SavoirFaire.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import SavoirFaireItem from "../../components/SavoirFaireItem/SavoirFaireItem"
 
 import Consultations from "../../icons/Consultations.svg"
@@ -11,16 +12,69 @@ import OQTF from "../../medias/OQTF.png"
 import "./SavoirFaire.css"
 
 function SavoirFaire() {
+    const [active0, setActive0] = useState(false);
+
+    const tabSavoiFaire = [
+        {
+            img: droitDuTravail,
+            title: "Le droit du travail",
+            description: "Le droit du travail est une discipline complexe qui encadre la vie du contrat de travail et plus largement la relation existant entre le salarié et son employeur, ainsi qu’avec les instances représentatives du personnel. Nos avocats assurent la défense des salariés devant le conseil de prud’hommes souhaitant contester leurs licenciements – qu’il s’agisse de ruptures disciplinaires (faute simple, faute grave, lourde), de licenciements pour inaptitude ou encore pour insuffisance professionnelle / de résultats. La société KLEROS a développé une expertise en matière de contentieux des entreprises à statuts (AIR FRANCE, SNCF, RATP, la Poste, etc…). KLEROS accompagne également les syndicats dans leurs actions relevant du contentieux des plans de sauvegarde de l’emploi (PSE), les licenciements collectifs pour motif économique, et les actions en dommages et intérêts pour atteintes aux intérêts de la profession. Nos avocats assistent également les syndicats dans le cadre du contentieux des élections professionnelles devant le pôle social du tribunal judiciaire territorialement compétent."
+        },
+        {
+            img: DroitProtectionSociale,
+            title: "Le droit de la protection sociale",
+            description: "Nos avocats accompagnent les salariés et assurés devant les différentes caisses d’assurance maladie ou de coordination pour leurs demande de reconnaissance des accidents du travail ou maladie professionnelles, de contestations des taux d’IPP et rentes y afférent, ainsi que pour tout type de contentieux mêlant des considérations de nature médicale. La société d’avocats KLEROS accompagne également les employeur faisant l’objet de procédures URSSAF et OFII pour le délit de travail dissimulé."
+            
+        },
+        {
+            img: OQTF,
+            title: "Le droit des étrangers - OQTF",
+            description: "KLEROS accompagne les étrangers demandeurs d’asile dans leurs démarches administratives ainsi que les étrangers en situation irrégulière demandant leur admission exceptionnelle au séjour, les demandes de regroupement familial. Lorsque l’étrangers fait l’objet d’une mesure d’éloignement (OQTF), nos équipes d’avocats se chargent d’en assurer la contestation devant les tribunaux administratifs." 
+        },
+        {
+            img: OQTF,
+            title: "Le droit des collectivités territoriales",
+            description: "Nos avocats interviennent régulièrement en soutien d’élus en matière de droit des collectivités territoriales, que ce soit en tant que conseil sur des opérations de contractualisation des politiques publiques (création de SEMOP, SPLA, SEM, etc), ou en contentieux des marchés publics (passation, exécution)."
+        },
+        {
+            img: OQTF,
+            title: "La réparation du préjudice corporel",
+            description: "Vous avez été victime d’un accident (de la route, du quotidien, agressions) ? la société d’avocats KLEROS peut vous représenter et vous permettre d’obtenir une indemnisation conforme aux barèmes des différents postes de préjudices prévus à la nomenclature DINTILHAC."
+        }
+    ]
     return(
         <div className="SavoirFaire">
             <h1>Savoir-faire</h1>
 
             <div className="savoirFaireItemContainer column">
-                <SavoirFaireItem image={droitDuTravail} title="Le droit du travail" description="Le droit du travail est une discipline complexe qui encadre la vie du contrat de travail et plus largement la relation existant entre le salarié et son employeur ..." />
-                <SavoirFaireItem image={DroitProtectionSociale} title="Le droit de la protection sociale" description="Nos avocats accompagnent les salariés et assurés devant les différentes caisses d’assurance maladie ou de coordination pour leurs demande ..." />
-                <SavoirFaireItem image={OQTF} title="Le droit des étrangers - OQTF" description="KLEROS accompagne les étrangers demandeurs d’asile dans leurs démarches administratives ainsi que les étrangers en situation irrégulière demandant ..." />
-                <SavoirFaireItem image={OQTF} title="Le droit des collectivités territoriales" description="Nos avocats interviennent régulièrement en soutien d’élus en matière de droit des collectivités territoriales, que ce soit en tant que conseil sur ..." />
-                <SavoirFaireItem image={OQTF} title="La réparation du préjudice corporel" description="Vous avez été victime d’un accident (de la route, du quotidien, agressions) ? La société d’avocats KLEROS peut vous représenter et vous permettre ..." />
+                {console.log(active0)}
+                <div onClick={() => setActive0(true)}>
+                    <SavoirFaireItem 
+                        image={tabSavoiFaire[0].img} 
+                        title= {tabSavoiFaire[0].title}
+                        description= {tabSavoiFaire[0].description}
+                    />
+                </div>
+                <SavoirFaireItem 
+                    image={tabSavoiFaire[1].img} 
+                    title= {tabSavoiFaire[1].title}
+                    description= {tabSavoiFaire[1].description}
+                />
+                <SavoirFaireItem 
+                    image={tabSavoiFaire[2].img} 
+                    title= {tabSavoiFaire[2].title}
+                    description= {tabSavoiFaire[2].description}
+                />
+                <SavoirFaireItem 
+                    image={tabSavoiFaire[3].img} 
+                    title= {tabSavoiFaire[3].title}
+                    description= {tabSavoiFaire[3].description}
+                />
+                <SavoirFaireItem 
+                    image={tabSavoiFaire[4].img} 
+                    title= {tabSavoiFaire[4].title}
+                    description= {tabSavoiFaire[4].description}
+                />
             </div>
 
             <div className="row resume">
